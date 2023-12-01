d9b7c56c8149c34e87098e03cb9dec785e3a2ff5
NaMe2te <vladyushashishkin81@gmail.com>
2023-12-01 14:31:39 +0300

    feat: lab-4

---
+++ login.js
@@ -0,0 +1,9 @@
+let home = document.getElementById('home');
+let cancelBtn = document.querySelector('.cancelbtn');
+
+let getHome = function() {
+    window.location = 'index.html';
+}
+
+home.addEventListener('click', getHome);
+cancelBtn.addEventListener('click', getHome);
---
+++ script.js
@@ -0,0 +1,6 @@
+let person_account = document.getElementById('person-account');
+
+person_account.addEventListener('click', () => {
+    console.log(1);
+    window.location = 'login.html';
+});
