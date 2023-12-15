5b2c88c5b8b7da0dd97f91a0f2b676b322427ac6
NaMe2te <vladyushashishkin81@gmail.com>
2023-12-15 04:15:55 +0300

    feat: lab-6

---
+++ pages/faq/faq.js
@@ -0,0 +1,45 @@
+let faqSummary = document.querySelector('.faq__summary');
+let answer = document.querySelector('.answer');
+let faqDetails = document.querySelector('.faq__details');
+let loader = document.querySelector('.lds-dual-ring');
+
+let home = document.getElementById('home');
+
+let getHome = function() {
+    window.location = '../../index.html';
+}
+home.addEventListener('click', getHome);
+
+faqSummary.addEventListener('click', () => {
+    if (!faqDetails.open){
+        if (answer.children.length != 0) {
+            while (answer.firstChild) {
+                if (answer.children.length == 0) {
+                    break;
+                }
+                answer.removeChild(answer.lastChild);
+            }
+        }
+    
+        loader.style.display = 'inline-block'
+        setTimeout(() => {
+            fetch(`https://jsonplaceholder.typicode.com/users?_start=1&_limit=${Math.floor(Math.random() * (8 - 4 + 1) + 4)}`)
+            .then(response => response.json()) 
+            .then(data => {
+                loader.style.display = 'none'
+                data.forEach(provider => {
+                    const providerEl = document.createElement('li');
+                    providerEl.textContent = provider.name + " - " + provider.email;
+                    answer.appendChild(providerEl);
+                })
+            })
+            .catch(error => {
+                loader.style.display = 'none'
+                const errorEl = document.createElement('li');
+                errorEl.textContent = "⚠ Что-то пошло не так";
+                errorEl.classList.add('error');
+                answer.appendChild(errorEl);
+            });
+        }, 2000);
+    }
+});
--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let person_account = document.getElementById('person-account');
 let buttonFooter = document.querySelector('.button__footer');
+let faqPage = document.getElementById('faq-page');
 
 person_account.addEventListener('click', () => {
     window.location = '/pages/login/login.html';
@@ -7,4 +8,8 @@ person_account.addEventListener('click', () => {
 
 buttonFooter.addEventListener('click', () => {
     window.location = '/pages/form/form.html';
+});
+
+faqPage.addEventListener('click', () => {
+    window.location = '/pages/faq/faq.html';
 });
